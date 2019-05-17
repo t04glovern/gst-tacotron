66b215617906b2293a9ec17b27988b89d5b35034
Nathan Glover <nathan@glovers.id.au>
2019-05-17 17:54:42 +0800

    updated to 405000

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "The masters must look to that, we can't." --reference_audio media/1_ref.wav
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "This is not the first time, nor the second, but it shall be the last." --reference_audio media/2_ref.wav
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "It is a very anxious period for her." --reference_audio media/3_ref.wav
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "At any other time this would have been felt dreadfully." --reference_audio media/4_ref.wav
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "You have not the smallest chance of moving me." --reference_audio media/5_ref.wav
-python eval.py --checkpoint logs-tacotron/model.ckpt-379000 --text "A tensorflow implementation of the Style Tokens Unsupervised Style Modeling, Control and Transfer in End-to-End Speech Synthesis" --reference_audio media/6_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "The masters must look to that, we can't." --reference_audio media/1_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "This is not the first time, nor the second, but it shall be the last." --reference_audio media/2_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "It is a very anxious period for her." --reference_audio media/3_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "At any other time this would have been felt dreadfully." --reference_audio media/4_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "You have not the smallest chance of moving me." --reference_audio media/5_ref.wav
+python eval.py --checkpoint logs-tacotron/model.ckpt-405000 --text "A tensorflow implementation of the Style Tokens Unsupervised Style Modeling, Control and Transfer in End-to-End Speech Synthesis" --reference_audio media/6_ref.wav
